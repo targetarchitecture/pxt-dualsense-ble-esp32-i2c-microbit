a2aa1029fb6e62ab66eba3aaaea6c0463767c0b4
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-05-09 13:39:19 +0100

    Removed the i2c queue and simplified code

--- main.ts
+++ main.ts
@@ -1,71 +1,3 @@
-/**
- * })
- */
-/**
- * basic.pause(1000)
- */
-/**
- * led.toggle(2, 2)
- */
-/**
- * )
- */
-/**
- * false
- */
-/**
- * NumberFormat.Int8LE,
- */
-/**
- * 0,
- */
-/**
- * i2cAddress,
- */
-/**
- * pins.i2cWriteNumber(
- */
-/**
- * basic.pause(1000)
- */
-/**
- * led.toggle(2, 2)
- */
-/**
- * )
- */
-/**
- * false
- */
-/**
- * NumberFormat.Int8LE,
- */
-/**
- * 1,
- */
-/**
- * i2cAddress,
- */
-/**
- * pins.i2cWriteNumber(
- */
-/**
- * basic.forever(function () {
- */
-
-input.onButtonPressed(Button.A, function () {
-    let ret = pins.i2cReadBuffer(i2cAddress, 15).toArray(NumberFormat.Int8LE)
-    for (let i = 0; i <= ret.length - 1; i++) {
-        charCode = ret[i]
-        if (charCode != -1) {
-            char = String.fromCharCode(charCode)
-            retTxt = retTxt.concat(char);
-        }
-    }
-    serial.writeString("recieved:" + retTxt)
-})
-
-
 
 let char2 = ""
 let charCode2 = 0
@@ -76,34 +8,19 @@ let retTxt2 = ""
 let retTxt = ""
 let i2cAddress = 121
 serial.redirectToUSB()
-let i2cCommands = [""]
-
-input.onButtonPressed(Button.B, function () {
-    i2cCommands.push("C:R,G,B")
-})
 
 basic.forever(function () {
-    sendi2c("1")
+    sendi2c("1");
     led.toggle(2, 2)
     basic.pause(1000)
     sendi2c("0")
     led.toggle(2, 2)
     basic.pause(1000)
-    sendi2cCommands();
-    let rcv = sendAndRecv("test");
+
+    let rcv = sendAndRecv("B");
 
     basic.showString(rcv);
 })
-
-function sendi2cCommands() {
-
-    while (i2cCommands.length > 0) {
-        i2cCommand = i2cCommands.shift()
-
-        sendi2c(i2cCommand)
-        basic.pause(10)
-    }
-}
 
 function sendi2c(command: String) {
 
@@ -131,6 +48,6 @@ function recvi2c() {
 
 function sendAndRecv(command: String) {
     sendi2c(command);
-    basic.pause(10)
+    basic.pause(5)
     return recvi2c();
 }
