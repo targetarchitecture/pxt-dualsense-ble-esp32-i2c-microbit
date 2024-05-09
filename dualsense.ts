a1a2cf16ce00307141925c4d5f7c0269a07a1ffd
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-05-09 13:50:51 +0100

    Moved code into Dualsense file

---
+++ dualsense.ts
@@ -0,0 +1,32 @@
+// Add your code here
+namespace dualsense {
+    export function sendi2c(command: String) {
+
+        let buff = pins.createBuffer(command.length);
+
+        for (let j = 0; j <= buff.length - 1; j++) {
+            buff.setNumber(NumberFormat.Int8LE, j, command.charCodeAt(j))
+        }
+
+        pins.i2cWriteBuffer(i2cAddress, buff)
+    }
+
+    export  function recvi2c() {
+        let ret = pins.i2cReadBuffer(i2cAddress, 15).toArray(NumberFormat.Int8LE)
+        for (let i = 0; i <= ret.length - 1; i++) {
+            charCode = ret[i]
+            if (charCode != -1) {
+                char = String.fromCharCode(charCode)
+                retTxt = retTxt.concat(char);
+            }
+        }
+        serial.writeString("recieved:" + retTxt)
+        return retTxt;
+    }
+
+    export  function sendAndRecv(command: String) {
+        sendi2c(command);
+        basic.pause(5)
+        return recvi2c();
+    }
+}
--- main.ts
+++ main.ts
@@ -10,44 +10,14 @@ let i2cAddress = 121
 serial.redirectToUSB()
 
 basic.forever(function () {
-    sendi2c("1");
+   dualsense.sendi2c("1");
     led.toggle(2, 2)
     basic.pause(1000)
-    sendi2c("0")
+    dualsense.sendi2c("0")
     led.toggle(2, 2)
     basic.pause(1000)
 
-    let rcv = sendAndRecv("B");
+    let rcv = dualsense.sendAndRecv("B");
 
     basic.showString(rcv);
 })
-
-function sendi2c(command: String) {
-
-    let buff = pins.createBuffer(command.length);
-
-    for (let j = 0; j <= buff.length - 1; j++) {
-        buff.setNumber(NumberFormat.Int8LE, j, command.charCodeAt(j))
-    }
-
-    pins.i2cWriteBuffer(i2cAddress, buff)
-}
-
-function recvi2c() {
-    let ret = pins.i2cReadBuffer(i2cAddress, 15).toArray(NumberFormat.Int8LE)
-    for (let i = 0; i <= ret.length - 1; i++) {
-        charCode = ret[i]
-        if (charCode != -1) {
-            char = String.fromCharCode(charCode)
-            retTxt = retTxt.concat(char);
-        }
-    }
-    serial.writeString("recieved:" + retTxt)
-    return retTxt;
-}
-
-function sendAndRecv(command: String) {
-    sendi2c(command);
-    basic.pause(5)
-    return recvi2c();
-}
