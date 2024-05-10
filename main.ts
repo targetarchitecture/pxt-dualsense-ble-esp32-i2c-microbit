7363ab9f4340867c601e05c544c92232b63c5ca1
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-05-10 10:03:17 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,51 @@
-serial.redirectToUSB()
-
-let DPAD ="";
-let BUTTONS = "";
-let AXISL = "";
-let AXISR = "";
-let BRAKE = "";
-let THROTTLE = "";
-let MISCBUTTONS = "";
-let GYRO = "";
-let ACCEL = "";
-
-basic.forever(function () {
-    dualsense.sendi2c("TEST:1");
-    //led.toggle(2, 2)
-    basic.pause(100)
-
-   dualsense.sendi2c("TEST:0")
-    //led.toggle(2, 2)
-   basic.pause(100)
-
-  DPAD=dualsense.sendAndRecv("DPAD");
-  //serial.writeString(DPAD)
-    basic.pause(50)
-    BUTTONS=  dualsense.sendAndRecv("BUTTONS");
-    basic.pause(50)
-    AXISL = dualsense.sendAndRecv("AXISL");
-    basic.pause(10)
-    AXISR = dualsense.sendAndRecv("AXISR");
-    basic.pause(10)
-    BRAKE = dualsense.sendAndRecv("BRAKE");
-    basic.pause(10)
-    THROTTLE = dualsense.sendAndRecv("THROTTLE");
-    basic.pause(10)
-    MISCBUTTONS = dualsense.sendAndRecv("MISCBUTTONS");
-    basic.pause(10)
-    GYRO = dualsense.sendAndRecv("GYRO");
-    basic.pause(10)
-    ACCEL = dualsense.sendAndRecv("ACCEL");
-    basic.pause(10)
-})
-
 input.onButtonPressed(Button.A, function () {
     dualsense.sendi2c("RUMBLE")
 })
-
 input.onButtonPressed(Button.B, function () {
     dualsense.sendi2c("LED:2")
-    dualsense.sendi2c("COLOUR:255,255,224")
-    dualsense.sendi2c("LED:4")
+dualsense.sendi2c("COLOUR:255,255,224")
+dualsense.sendi2c("LED:4")
 })
-
+let ACCEL = ""
+let GYRO = ""
+let MISCBUTTONS = ""
+let THROTTLE = ""
+let BRAKE = ""
+let AXISR = ""
+let AXISL = ""
+let BUTTONS = ""
+let DPAD = ""
+serial.redirectToUSB()
+music.setBuiltInSpeakerEnabled(true)
+//music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+//     dualsense.sendi2c("TEST:1");
+// // led.toggle(2, 2)
+//     basic.pause(50)
+//     dualsense.sendi2c("TEST:0")
+// // led.toggle(2, 2)
+//     basic.pause(50)
+//     DPAD = dualsense.sendAndRecv("DPAD");
+// // serial.writeString(DPAD)
+//     basic.pause(50)
+    BUTTONS = dualsense.sendAndRecv("BUTTONS");
+basic.pause(100)
+//     AXISL = dualsense.sendAndRecv("AXISL");
+// basic.pause(50)
+//     AXISR = dualsense.sendAndRecv("AXISR");
+// basic.pause(50)
+//     BRAKE = dualsense.sendAndRecv("BRAKE");
+// basic.pause(50)
+//     THROTTLE = dualsense.sendAndRecv("THROTTLE");
+// basic.pause(50)
+//     MISCBUTTONS = dualsense.sendAndRecv("MISCBUTTONS");
+// basic.pause(50)
+//     GYRO = dualsense.sendAndRecv("GYRO");
+// basic.pause(50)
+//     ACCEL = dualsense.sendAndRecv("ACCEL");
+// basic.pause(50)
+})
+basic.forever(function () {
+    serial.writeLine("" + (BUTTONS))
+    basic.pause(1000)
+})
