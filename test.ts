e24564aa6ea1e085dba0ee4f912783d600293ce9
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-05-16 13:11:30 +0100

    Update pxt.json, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,20 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+/*
+dualsense.startDualSense();
+
+input.onButtonPressed(Button.A, function () {
+    dualsense.colour(randint(0, 255), randint(0, 255), randint(0, 255))
+})
+
+dualsense.onClicked(dualsense.Buttons.A, function () {
+    dualsense.rumble();
+})
+
+dualsense.onClicked(dualsense.Buttons.X, function () {
+    dualsense.led(randint(1, 4))
+})
+
+
+dualsense.onClicked(dualsense.Buttons.B, function () {
+    dualsense.colour(randint(0, 255), randint(0, 255), randint(0, 255))
+})
+*/
