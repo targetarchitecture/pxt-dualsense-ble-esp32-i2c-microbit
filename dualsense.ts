9b9ad2e7199ca49ad7e991a5516a2e63d6a0a6ac
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-05-16 13:04:31 +0100

    Update main.ts, dualsense.ts

--- dualsense.ts
+++ dualsense.ts
@@ -23,10 +23,6 @@ namespace dualsense {
     let R2_PRESSED = 0;
     let LTHUMB_PRESSED = 0;
     let RTHUMB_PRESSED = 0;
-    let SYSTEM_PRESSED = 0;
-    let SELECT_PRESSED = 0;
-    let START_PRESSED = 0;
-    let CAPTURE_PRESSED = 0;
 
     let LAXISX = 0;
     let LAXISY = 0;
@@ -44,77 +40,65 @@ namespace dualsense {
         }
 
         pins.i2cWriteBuffer(i2cAddress, buff)
-        buff = null;
     }
 
-    // function recvi2c() {
-    //     let retTxt = ""
-    //     let ret = pins.i2cReadBuffer(i2cAddress, 32).toArray(NumberFormat.Int8LE)
-    //     for (let i = 0; i <= ret.length - 1; i++) {
-    //         if (ret[i] != -1) {
-    //             retTxt = retTxt.concat(String.fromCharCode(ret[i]));
-    //         }
-    //     }
-    //     ret = null;
-    //     return retTxt;
-    // }
-
-    function sendAndRecv(command: string) {
-        let buff = pins.createBuffer(command.length);
-        let retTxt = "";
-
-        for (let j = 0; j <= buff.length - 1; j++) {
-            buff.setNumber(NumberFormat.Int8LE, j, command.charCodeAt(j))
-        }
-
-        pins.i2cWriteBuffer(i2cAddress, buff);
-        basic.pause(5);
-        let readBuffer = pins.i2cReadBuffer(i2cAddress, 32).toArray(NumberFormat.Int8LE);
-
-        for (let i = 0; i <= readBuffer.length - 1; i++) {
-            if (readBuffer[i] != -1) {
-                retTxt = retTxt.concat(String.fromCharCode(readBuffer[i]));
+    function recieveI2CData() {
+        let retTxt = ""
+        let ret = pins.i2cReadBuffer(i2cAddress, 32).toArray(NumberFormat.Int8LE)
+        for (let i = 0; i <= ret.length - 1; i++) {
+            if (ret[i] != -1) {
+                retTxt = retTxt.concat(String.fromCharCode(ret[i]));
             }
         }
+        retTxt = retTxt.replace("\n", "");
 
-        serial.writeLine("sendAndRecv:" + command);
-        serial.writeLine("sendAndRecv (" + retTxt.replace("\n","") + ")");
+        //serial.writeLine("recieveI2CData: (" + retTxt + ")");
 
-        return retTxt;
+        let command = retTxt.split(",", 1)
+
+        if (command[0] == "DPAD") {
+            dpadState(retTxt);
+        } else if (command[0] == "BUTTONS") {
+            buttonState(retTxt);
+        } else if (command[0] == "AXISL") {
+            axisLeftState(retTxt);
+        } else if (command[0] == "AXISR") {
+            axisRightState(retTxt);
+        } else if (command[0] == "TRIGGERS") {
+            triggerState(retTxt);
+        } else if (command[0] == "GYRO") {
+            //  dpadState(retTxt);
+        } else if (command[0] == "ACCEL") {
+            // dpadState(retTxt);
+        }
     }
 
-    // function parseInt(value: string) {
-    //     let i = parseInt(value);
-    //     if (i == 1) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
+
+
+    /**
+     * Start using Playstation Dualsense
+     */
+    //% weight=10
+    //% block="Start Controller"   
+    export function startDualSense() {
+        basic.forever(function () {
+            recieveI2CData();
+            basic.pause(10);
+        })
+    }
 
     export const btnOffset = 1000;
 
-    /**
-     * Check for button states on the Playstation Dualsense
-     */
+    function buttonState(i2cData: string) {
+        let buttons = i2cData.split(",");
 
-    //% weight=10
-    //% block="button state"   
-    export function buttonState() {
-        let returnedValue = sendAndRecv("BUTTONS");
-        let buttons = returnedValue.split(",");
+        //serial.writeLine("buttonState:" + i2cData);
+        //  serial.writeLine(control.millis() + "> Button:A,parseInt:" + buttons[0] + ",Pressed:" + A_PRESSED);
 
-        serial.writeLine("buttonState:" + returnedValue);
-      //  serial.writeLine(control.millis() + "> Button:A,parseInt:" + buttons[0] + ",Pressed:" + A_PRESSED);
-
-        if (A_PRESSED == 0 && parseInt(buttons[1]) == 1) {
-             serial.writeLine(control.millis() + "> Button:A,parseInt:" + parseInt(buttons[1]) + ",Pressed:" + A_PRESSED);
-            A_PRESSED = 1;
+        if (A_PRESSED == 0 && parseInt(buttons[Buttons.A]) == 1) {
+            //serial.writeLine(control.millis() + "> Button:A,parseInt:" + parseInt(buttons[Buttons.A]) + ",Pressed:" + A_PRESSED);
 
             control.raiseEvent(PS5_BUTTON_CLICKED + Buttons.A, Buttons.A + btnOffset);
-        }
-        else {
-            A_PRESSED = 0;
         }
 
         if (B_PRESSED == 0 && parseInt(buttons[Buttons.B]) == 1) {
@@ -143,94 +127,60 @@ namespace dualsense {
 
         } else if (RTHUMB_PRESSED == 0 && parseInt(buttons[Buttons.RTHUMB]) == 1) {
             control.raiseEvent(PS5_BUTTON_CLICKED + Buttons.RTHUMB, Buttons.RTHUMB + btnOffset);
+        } 
 
-        } else if (SYSTEM_PRESSED == 0 && parseInt(buttons[Buttons.SYSTEM]) == 1) {
-            control.raiseEvent(PS5_BUTTON_CLICKED + Buttons.SYSTEM, Buttons.SYSTEM + btnOffset);
-
-        } else if (SELECT_PRESSED == 0 && parseInt(buttons[Buttons.SELECT]) == 1) {
-            control.raiseEvent(PS5_BUTTON_CLICKED + Buttons.SELECT, Buttons.SELECT + btnOffset);
-
-        } else if (START_PRESSED == 0 && parseInt(buttons[Buttons.START]) == 1) {
-            control.raiseEvent(PS5_BUTTON_CLICKED + Buttons.START, Buttons.START + btnOffset);
-
-        } else if (CAPTURE_PRESSED == 0 && parseInt(buttons[Buttons.CAPTURE]) == 1) {
-            control.raiseEvent(PS5_BUTTON_CLICKED + Buttons.CAPTURE, Buttons.CAPTURE + btnOffset);
-        }
-
-        //  A_PRESSED = parseInt(buttons[0]);
-        B_PRESSED = parseInt(buttons[1]);
-        X_PRESSED = parseInt(buttons[2]);
-        Y_PRESSED = parseInt(buttons[3]);
-        L1_PRESSED = parseInt(buttons[4]);
-        L2_PRESSED = parseInt(buttons[5]);
-        R1_PRESSED = parseInt(buttons[6]);
-        R2_PRESSED = parseInt(buttons[7]);
-        LTHUMB_PRESSED = parseInt(buttons[8]);
-        RTHUMB_PRESSED = parseInt(buttons[9]);
-        SYSTEM_PRESSED = parseInt(buttons[10]);
-        SELECT_PRESSED = parseInt(buttons[11]);
-        START_PRESSED = parseInt(buttons[12]);
-        CAPTURE_PRESSED = parseInt(buttons[13]);
+        A_PRESSED = parseInt(buttons[Buttons.A]);
+        B_PRESSED = parseInt(buttons[Buttons.B]);
+        X_PRESSED = parseInt(buttons[Buttons.X]);
+        Y_PRESSED = parseInt(buttons[Buttons.Y]);
+        L1_PRESSED = parseInt(buttons[Buttons.L1]);
+        L2_PRESSED = parseInt(buttons[Buttons.L2]);
+        R1_PRESSED = parseInt(buttons[Buttons.R1]);
+        R2_PRESSED = parseInt(buttons[Buttons.R2]);
+        LTHUMB_PRESSED = parseInt(buttons[Buttons.LTHUMB]);
+        RTHUMB_PRESSED = parseInt(buttons[Buttons.RTHUMB]);
     }
 
-
-    /**
-     * Check for DPad states on the Playstation Dualsense
-     */
-    //% weight=20
-    //% block="dpad state"   
-    export function dpadState() {
-        let returnedValue = sendAndRecv("DPAD");
-        let directions = returnedValue.split(",");
-        DPAD_UP_PRESSED = parseInt(directions[0]);
-        DPAD_UP_RIGHT_PRESSED = parseInt(directions[1]);
-        DPAD_RIGHT_PRESSED = parseInt(directions[2]);
-        DPAD_DOWN_RIGHT_PRESSED = parseInt(directions[3]);
-        DPAD_DOWN_PRESSED = parseInt(directions[4]);
-        DPAD_DOWN_LEFT_PRESSED = parseInt(directions[5]);
-        DPAD_LEFT_PRESSED = parseInt(directions[6]);
-        DPAD_UP_LEFT_PRESSED = parseInt(directions[7]);
+    function dpadState(i2cData: string) {
+        let directions = i2cData.split(",");
+        let command = directions[0];
+        DPAD_UP_PRESSED = parseInt(directions[1]);
+        DPAD_UP_RIGHT_PRESSED = parseInt(directions[2]);
+        DPAD_RIGHT_PRESSED = parseInt(directions[3]);
+        DPAD_DOWN_RIGHT_PRESSED = parseInt(directions[4]);
+        DPAD_DOWN_PRESSED = parseInt(directions[5]);
+        DPAD_DOWN_LEFT_PRESSED = parseInt(directions[6]);
+        DPAD_LEFT_PRESSED = parseInt(directions[7]);
+        DPAD_UP_LEFT_PRESSED = parseInt(directions[8]);
     }
 
-    /**
-     * Check for Left stick states on the Playstation Dualsense
-     */
-    //% weight=30
-    //% block="left stick state" 
-    export function axisLeftState() {
-        let returnedValue = sendAndRecv("AXISL");
-        let AXISL = returnedValue.split(",");
-        LAXISX = parseInt(AXISL[0]);
-        LAXISY = parseInt(AXISL[1]);
+    function axisLeftState(i2cData: string) {
+        let AXISL = i2cData.split(",");
+        let command = AXISL[0];
+        LAXISX = parseInt(AXISL[1]);
+        LAXISY = parseInt(AXISL[2]);
     }
 
-    /**
-     * Check for Right stick states on the Playstation Dualsense
-     */
-    //% weight=40
-    //% block="right stick state" 
-    export function axisRightState() {
-        let returnedValue = sendAndRecv("AXISR");
-        let AXISR = returnedValue.split(",");
-        LAXISX = parseInt(AXISR[0]);
-        LAXISY = parseInt(AXISR[1]);
+    function axisRightState(i2cData: string) {
+        let AXISR = i2cData.split(",");
+        let command = AXISR[0];
+        LAXISX = parseInt(AXISR[1]);
+        LAXISY = parseInt(AXISR[2]);
     }
 
-    /**
-     * Check for trigger states on the Playstation Dualsense
-     */
-    //% weight=50
-    //% block="trigger state" 
-    export function triggerState() {
-        let returnedValue = sendAndRecv("TRIGGERS");
-        let TRIGGERS = returnedValue.split(",");
+    function triggerState(i2cData: string) {
+        let TRIGGERS = i2cData.split(",");
+        let command = TRIGGERS[0];
 
-        if (parseInt(TRIGGERS[0]) != BRAKE) {
-            BRAKE = parseInt(TRIGGERS[0]);
+        if (parseInt(TRIGGERS[1]) != BRAKE) {
+            BRAKE = parseInt(TRIGGERS[1]);
             control.raiseEvent(PS5_BRAKE_VALUE_CHANGED, BRAKE);
         }
 
-        THROTTLE = parseInt(TRIGGERS[1]);
+        if (parseInt(TRIGGERS[2]) != THROTTLE) {
+            THROTTLE = parseInt(TRIGGERS[2]);
+            control.raiseEvent(PS5_THROTTLE_VALUE_CHANGED, THROTTLE);
+        }
     }
 
     /**
@@ -287,37 +237,30 @@ namespace dualsense {
     }
 
     const PS5_BRAKE_VALUE_CHANGED = 5010;
-    const PS5_BUTTON_CLICKED = 5020;
+    const PS5_THROTTLE_VALUE_CHANGED = 5020;
+    const PS5_BUTTON_CLICKED = 5090;
 
     export enum Buttons {
         //% block=" A"    
-        A = 0,
+        A = 1,
         //% block="B"
-        B = 1,
+        B = 2,
         //% block="X"
-        X = 2,
+        X = 3,
         //% block="Y"
-        Y = 3,
+        Y = 4,
         //% block="L1"
-        L1 = 4,
+        L1 = 5,
         //% block="L2"
-        L2 = 5,
+        L2 = 6,
         //% block="R1"
-        R1 = 6,
+        R1 = 7,
         //% block="R2"
-        R2 = 7,
+        R2 = 8,
         //% block="Left Thumb"
-        LTHUMB = 8,
+        LTHUMB = 9,
         //% block="Right Thumb" 
-        RTHUMB = 9,
-        //% block="System" 
-        SYSTEM = 10,
-        //% block="Select"
-        SELECT = 11,
-        //% block="Start"
-        START = 12,
-        //% block="Capture" 
-        CAPTURE = 13
+        RTHUMB = 10
     }
 
     /**
--- main.ts
+++ main.ts
@@ -1,21 +1,20 @@
-basic.forever(function () {
-    // New Dualsense library
 
-   dualsense.axisRightState()
-    basic.pause(500)
-    dualsense.buttonState()
-    basic.pause(500)
-})
-
+dualsense.startDualSense();
 
 input.onButtonPressed(Button.A, function() {
     dualsense.colour(randint(0, 255), randint(0, 255), randint(0, 255))
 })
 
-// dualsense.onClicked(dualsense.Buttons.A, function() {
-//     dualsense.rumble();
-// })
+dualsense.onClicked(dualsense.Buttons.A, function() {
+    dualsense.rumble();
+})
 
-// dualsense.onClicked(dualsense.Buttons.X, function () {
-//     dualsense.led(randint(1,4))
-// })
+dualsense.onClicked(dualsense.Buttons.X, function () {
+    dualsense.led(randint(1,4))
+})
+
+
+dualsense.onClicked(dualsense.Buttons.B, function () {
+    dualsense.colour(randint(0, 255), randint(0, 255), randint(0, 255))
+})
+
